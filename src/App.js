dbe54fab216794bd617d1f7a7595c9ca149eb43f
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-11-03 17:21:06 +0100

    add link to film api

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MyFilmApp</h1>
+      <div className="search">
+        <input placeholder="Search for movies" value="Superman"></input>
+      </div>
+    </div>
+  );
+};
+
+export default App;
